3b5475de0e6c05633077f478f9affa74357f2475
atomsk-0 <wexberkyhd@gmail.com>
2024-12-06 11:25:08 +0200

    Remove all Marshal.* functions and move to own also added new option to allow user select multiple folders and files see Basic Usage project for more information

--- src/NativeFileDialogNET/NativeFileDialog.cs
+++ src/NativeFileDialogNET/NativeFileDialog.cs
@@ -17,7 +17,15 @@ public unsafe class NativeFileDialog : IDisposable
         }
     }
 
+    // To not break changes as Open with string[] would be now the main method to use
     public DialogResult Open(out string? output, string? defaultPath = null, string? defaultName = null)
+    {
+        DialogResult result = Open(out string[]? outputs, defaultPath, defaultName);
+        output = outputs?[0];
+        return result;
+    }
+
+    public DialogResult Open(out string[]? output, string? defaultPath = null, string? defaultName = null)
     {
         output = null;
         if (initialized == false) throw new InvalidOperationException("NativeFileDialog is not initialized.");
@@ -26,68 +34,137 @@ public unsafe class NativeFileDialog : IDisposable
             case DialogMode.SelectFile:
             {
                 NfdResult result;
-                NfdU8FilterItem[] nativeFilterItems = Util.ConvertFilterItemsToNative(CollectionsMarshal.AsSpan(filterItems));
+                NfdU8FilterItem[] nativeFilterItems = ConvertFilterItemsToNative(CollectionsMarshal.AsSpan(filterItems));
                 sbyte* selectedPath = null;
                 fixed(NfdU8FilterItem* filterItemsPtr = nativeFilterItems)
                 {
-                    sbyte* defaultPathPtr = Util.ConvertStringToPointer(defaultPath);
+                    sbyte* defaultPathPtr = (sbyte*)NativeMemoryUtils.StringToUTF8Ptr(defaultPath);
                     result = Bindings.OpenDialog(&selectedPath, filterItemsPtr, (uint)nativeFilterItems.Length, defaultPathPtr);
                     if (result == NfdResult.Okay)
                     {
-                        output = Marshal.PtrToStringUTF8((IntPtr)selectedPath);
+                        string? rt = NativeMemoryUtils.PtrToUTF8String((byte*)selectedPath);
+                        if (rt != null) output = [rt];
                         Bindings.FreePath(selectedPath);
                     }
-                    Util.FreeStringPointer(defaultPathPtr);
+                    NativeMemoryUtils.Free(defaultPathPtr);
                 }
                 foreach (var item in nativeFilterItems)
                 {
-                    Util.FreeStringPointer(item.Name);
-                    Util.FreeStringPointer(item.Spec);
+                    NativeMemoryUtils.Free(item.Name);
+                    NativeMemoryUtils.Free(item.Spec);
                 }
-                return MapResult(result);
+                return mapResult(result);
+            }
+            case DialogMode.SelectMultipleFiles:
+            {
+                NfdResult result;
+                NfdU8FilterItem[] nativeFilterItems = ConvertFilterItemsToNative(CollectionsMarshal.AsSpan(filterItems));
+                void* selectedPaths = null;
+                fixed(NfdU8FilterItem* filterItemsPtr = nativeFilterItems)
+                {
+                    sbyte* defaultPathPtr = (sbyte*)NativeMemoryUtils.StringToUTF8Ptr(defaultPath);
+                    result = Bindings.OpenDialogMultiple(&selectedPaths, filterItemsPtr, (uint)nativeFilterItems.Length, defaultPathPtr);
+                    if (result == NfdResult.Okay)
+                    {
+                        int numPaths;
+                        Bindings.PathSetGetCount(selectedPaths, &numPaths);
+
+                        List<string> strings = [];
+                        for (int i = 0; i < numPaths; i++)
+                        {
+                            sbyte* path = null;
+                            Bindings.PathSetGetPath(selectedPaths, i, &path);
+                            string? rt = NativeMemoryUtils.PtrToUTF8String((byte*)path);
+                            if (rt != null) strings.Add(rt);
+                            Bindings.PathSetFreePath(path);
+                        }
+                        if (strings.Count > 0)
+                        {
+                            output = strings.ToArray();
+                        }
+                        Bindings.PathSetFreePath(selectedPaths);
+                    }
+                    NativeMemoryUtils.Free(defaultPathPtr);
+                }
+                foreach (var item in nativeFilterItems)
+                {
+                    NativeMemoryUtils.Free(item.Name);
+                    NativeMemoryUtils.Free(item.Spec);
+                }
+                return mapResult(result);
             }
             case DialogMode.SelectFolder:
             {
                 sbyte* selectedPath = null;
-                sbyte* defaultPathPtr = Util.ConvertStringToPointer(defaultPath);
+                sbyte* defaultPathPtr = (sbyte*)NativeMemoryUtils.StringToUTF8Ptr(defaultPath);
                 NfdResult result = Bindings.PickFolder(&selectedPath, defaultPathPtr);
                 if (result == NfdResult.Okay)
                 {
-                    output = Marshal.PtrToStringUTF8((IntPtr)selectedPath);
+                    string? rt = NativeMemoryUtils.PtrToUTF8String((byte*)selectedPath);
+                    if (rt != null) output = [rt];
                     Bindings.FreePath(selectedPath);
                 }
-                Util.FreeStringPointer(defaultPathPtr);
-                return MapResult(result);
+                NativeMemoryUtils.Free(defaultPathPtr);
+                return mapResult(result);
+            }
+            case DialogMode.SelectMultipleFolders:
+            {
+                void* selectedPaths = null;
+                sbyte* defaultPathPtr = (sbyte*)NativeMemoryUtils.StringToUTF8Ptr(defaultPath);
+                NfdResult result = Bindings.PickFolderMultiple(&selectedPaths, defaultPathPtr);
+                if (result == NfdResult.Okay)
+                {
+                    int numPaths;
+                    Bindings.PathSetGetCount(selectedPaths, &numPaths);
+
+                    List<string> strings = [];
+                    for (int i = 0; i < numPaths; i++)
+                    {
+                        sbyte* path = null;
+                        Bindings.PathSetGetPath(selectedPaths, i, &path);
+                        string? rt = NativeMemoryUtils.PtrToUTF8String((byte*)path);
+                        if (rt != null) strings.Add(rt);
+                        Bindings.PathSetFreePath(path);
+                    }
+                    if (strings.Count > 0)
+                    {
+                        output = strings.ToArray();
+                    }
+                    Bindings.PathSetFreePath(selectedPaths);
+                }
+                NativeMemoryUtils.Free(defaultPathPtr);
+                return mapResult(result);
             }
             case DialogMode.SaveFile:
             {
                 NfdResult result;
-                NfdU8FilterItem[] nativeFilterItems = Util.ConvertFilterItemsToNative(CollectionsMarshal.AsSpan(filterItems));
+                NfdU8FilterItem[] nativeFilterItems = ConvertFilterItemsToNative(CollectionsMarshal.AsSpan(filterItems));
                 sbyte* selectedPath = null;
                 fixed(NfdU8FilterItem* filterItemsPtr = nativeFilterItems)
                 {
-                    sbyte* defaultPathPtr = Util.ConvertStringToPointer(defaultPath);
-                    sbyte* defaultNamePtr = Util.ConvertStringToPointer(defaultName);
+                    sbyte* defaultPathPtr = (sbyte*)NativeMemoryUtils.StringToUTF8Ptr(defaultPath);
+                    sbyte* defaultNamePtr = (sbyte*)NativeMemoryUtils.StringToUTF8Ptr(defaultName);
                     result = Bindings.SaveDialog(&selectedPath, filterItemsPtr, (uint)nativeFilterItems.Length, defaultPathPtr, defaultNamePtr);
                     if (result == NfdResult.Okay)
                     {
-                        output = Marshal.PtrToStringUTF8((IntPtr)selectedPath);
+                        string? rt = NativeMemoryUtils.PtrToUTF8String((byte*)selectedPath);
+                        if (rt != null) output = [rt];
                         Bindings.FreePath(selectedPath);
                     }
-                    Util.FreeStringPointer(defaultPathPtr);
-                    Util.FreeStringPointer(defaultNamePtr);
+                    NativeMemoryUtils.Free(defaultPathPtr);
+                    NativeMemoryUtils.Free(defaultNamePtr);
                 }
                 foreach (var item in nativeFilterItems)
                 {
-                    Util.FreeStringPointer(item.Name);
-                    Util.FreeStringPointer(item.Spec);
+                    NativeMemoryUtils.Free(item.Name);
+                    NativeMemoryUtils.Free(item.Spec);
                 }
-                return MapResult(result);
+                return mapResult(result);
             }
         }
         return DialogResult.Error;
 
-        static DialogResult MapResult(NfdResult result)
+        static DialogResult mapResult(NfdResult result)
             => result switch
             {
                 NfdResult.Okay => DialogResult.Okay,
@@ -117,6 +194,13 @@ public unsafe class NativeFileDialog : IDisposable
         return this;
     }
 
+    public NativeFileDialog AllowMultiple()
+    {
+        dialogMode = dialogMode == DialogMode.SelectFolder ? DialogMode.SelectMultipleFolders : DialogMode.SelectMultipleFiles;
+        filterItems.Clear();
+        return this;
+    }
+
     public NativeFileDialog AddFilter(string name, string spec)
     {
         filterItems.Add(new FilterItem(name, spec));
@@ -132,6 +216,20 @@ public unsafe class NativeFileDialog : IDisposable
         }
         filterItems.Clear();
         GC.SuppressFinalize(this);
+    }
+
+    internal static NfdU8FilterItem[] ConvertFilterItemsToNative(in Span<FilterItem> filterItems)
+    {
+        NfdU8FilterItem[] nativeFilterItems = new NfdU8FilterItem[filterItems.Length];
+        for (int i = 0; i < filterItems.Length; i++)
+        {
+            nativeFilterItems[i] = new NfdU8FilterItem
+            {
+                Name = (sbyte*)NativeMemoryUtils.StringToUTF8Ptr(filterItems[i].Name),
+                Spec = (sbyte*)NativeMemoryUtils.StringToUTF8Ptr(filterItems[i].Spec)
+            };
+        }
+        return nativeFilterItems;
     }
 
     ~NativeFileDialog() => Dispose();
@@ -153,6 +251,8 @@ public enum DialogResult
 internal enum DialogMode
 {
     SelectFile,
+    SelectMultipleFiles,
     SelectFolder,
+    SelectMultipleFolders,
     SaveFile,
 }
